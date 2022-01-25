bb47d801b6e5a526e57a1bf346c19b17086d79fa
marty freeman <marty.freeman@healthequity.com>
2022-01-25 13:47:29 -0600

    Revert "Module 5"
    This reverts commit 4e0b517cd4daf8740c1142aeedea9b1cafd5e7b0.

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -12,6 +12,7 @@ namespace StarChart.Controllers
     public class CelestialObjectController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
+        private object celestialObject;
 
         public CelestialObjectController(ApplicationDbContext context)
         {
@@ -26,30 +27,6 @@ namespace StarChart.Controllers
             return NotFound();
             celestialObject.Satellites = _context.CelestialObjects.Where(e => e.OrbitedObjectId == id).ToList();
             return Ok(celestialObject);
-        }
-
-        [HttpGet("{name")]
-        public IActionResult GetByName(string name)
-
-        {
-            var celestialObjects = _context.CelestialObjects.Where(e => e.Name == name).ToList();
-            if (!celestialObjects.Any())
-                return NotFound();
-            foreach(var celestialObject in celestialObjects)
-            {
-                celestialObject.Satellites = _context.CelestialObjects.Where(e => e.OrbitedObjectId == celestialObject.Id).ToList();
-            }
-            return Ok(celestialObjects);
-        }
-
-        public IActionResult GetAll()
-        {
-            var celestialObjects = _context.CelestialObjects.ToList();
-            foreach(var celestialObject in celestialObjects)
-            {
-                celestialObject.Satellites = _context.CelestialObjects.Where(e => e.OrbitedObjectId == celestialObject.Id).ToList();
-            }
-            return Ok(celestialObjects);
         }
 
     }
