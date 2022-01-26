3c0a6352cf5f00a4c39d04d8473bcf544056e287
marty freeman <marty.freeman@healthequity.com>
2022-01-26 12:28:43 -0600

    Module 3

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+/// <summary>
+/// <Module 3 Celestial Model>
 namespace StarChart.Models
 {
     public class CelestialObject
